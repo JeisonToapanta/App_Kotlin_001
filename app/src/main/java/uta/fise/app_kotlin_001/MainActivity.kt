2d5f8e79eed9df3e52dc75083b8b3a748e790f0f
JeisonToapanta <0995356573123jeick@gmail.com>
2023-06-16 17:14:53 -0500

    Kotlin 16-06-2023 [2]

--- app/src/main/java/uta/fise/app_kotlin_001/MainActivity.kt
+++ app/src/main/java/uta/fise/app_kotlin_001/MainActivity.kt
@@ -1,32 +1,41 @@
 package uta.fise.app_kotlin_001
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import uta.fise.app_kotlin_001.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //YA NO VA ASI EL CONTENTVIEW
+        //setContentView(R.layout.activity_main)
+        //AHORA VA ASI
+        val binding = ActivityMainBinding.inflate(layoutInflater)  //Estoy apuntando a la raiz del layout o activity padre
+        setContentView(binding.root)  //ESTO TOCA HACER PARA CADA FORMULARIO PARA REFERENCIAR
 
-        val editeTextNumberOne = findViewById<EditText>(R.id.editTextNumberOne);
-        val editeTextNumberTwo = findViewById<EditText>(R.id.editTextNumberTwo);
+        //val editeTextNumberOne = findViewById<EditText>(R.id.editTextNumberOne);
+        //val editTextNumberOne = binding.editTextNumberOne
 
-        val editeTextResult = findViewById<EditText>(R.id.editTextResult);
-        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        //val editeTextNumberTwo = findViewById<EditText>(R.id.editTextNumberTwo);
+        //val editTextNumberTwo = binding.editTextNumberTwo
 
-        buttonAdd.setOnClickListener{
-            val a = editeTextNumberOne.text.toString().toInt()
-            val b = editeTextNumberTwo.text.toString().toInt()
+        //val editeTextResult = findViewById<EditText>(R.id.editTextResult);
+        //val editTextResult = binding.editTextResult
 
+        //val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        //val buttonAdd = binding.buttonAdd
+
+        binding.buttonAdd.setOnClickListener{
+            //val a = editeTextNumberOne.text.toString().toInt()
+            val a = binding.editTextNumberOne.text.toString().toInt()
+            //val b = editeTextNumberTwo.text.toString().toInt()
+            val b = binding.editTextNumberTwo.text.toString().toInt()
             val add = a + b
 
-            editeTextResult.setText(add.toString())
+            binding.editTextResult.setText(add.toString())
         }
-
 
     }
 }
